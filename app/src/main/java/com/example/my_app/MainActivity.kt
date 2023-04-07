ca9a9056e39305c8e76b48869eaa5ae87c3eb875
Akay <shroffakshit@gmail.com>
2023-04-07 17:17:39 +0530

    Implicit Intent and Webview Added

--- app/src/main/java/com/example/my_app/MainActivity.kt
+++ app/src/main/java/com/example/my_app/MainActivity.kt
@@ -4,18 +4,19 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Layout
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ResourceAsColor")
+    @SuppressLint("ResourceAsColor", "WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
 //        *** TO MAKE TOAST MSG WHEN BUTTON ARE PRESSED ***
 
@@ -43,6 +44,12 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val runWebsite = findViewById<Button>(R.id.runWeb)
+
+        runWebsite.setOnClickListener{
+            intent = Intent(applicationContext , MainActivity3::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/my_app/MainActivity2.kt
+++ app/src/main/java/com/example/my_app/MainActivity2.kt
@@ -1,13 +1,37 @@
 package com.example.my_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val cameraButton = findViewById<CardView>(R.id.openCame)
+        val webButton = findViewById<CardView>(R.id.openWebsite)
+        val callButton = findViewById<CardView>(R.id.openCalling)
 
+        cameraButton.setOnClickListener{
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+        webButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/Akay-Code")
+            startActivity(intent)
+        }
+        callButton.setOnClickListener{
+
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:" + "8178460227" )
+            startActivity(intent)
+        }
     }
 }
